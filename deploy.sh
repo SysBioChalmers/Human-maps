b05b40c772262ac26fa0e1da978bedc26906221e
Mihail Anton <petre@chalmers.se>
2019-05-06 12:53:05 +0200

    update hardcoded model folder

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rsync -auvp *.svg ftp.icsb.chalmers.se:/home/"$(whoami)"/maps/
-ssh -t ftp.icsb.chalmers.se "sudo mv /home/"$(whoami)"/maps/*.svg /var/www/html/.maps/hmr2/"
+ssh -t ftp.icsb.chalmers.se "sudo mv /home/"$(whoami)"/maps/*.svg /var/www/html/.maps/human1/"
