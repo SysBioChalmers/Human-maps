ef6d764955641bff9f177d006d7ecd4fccb29f10
Mihail Anton <petre@chalmers.se>
2019-05-09 10:22:34 +0200

    return error code from ssh command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rsync -auvp *.svg ftp.icsb.chalmers.se:/home/"$(whoami)"/maps/
-ssh -t ftp.icsb.chalmers.se "sudo mv /home/"$(whoami)"/maps/*.svg /var/www/html/.maps/human1/"
+ssh -t ftp.icsb.chalmers.se "sudo mv /home/"$(whoami)"/maps/*.svg /var/www/html/.maps/human1/" && echo $?
