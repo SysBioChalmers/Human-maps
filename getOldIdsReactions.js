f8441f21dce195b8517ad5a402fe24b252ff2b9d
Ingrid Hyltander <ingrid.hyltander@nbis.se>
2021-07-14 14:57:12 +0200

    feat: record which reaction and metabolite IDs have not been replaced in the svg:s for Human-GEM. The not_replaced_x_ids.txt have been generated with getOldIdsX.js

---
+++ getOldIdsReactions.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+const fileResult = 'not_replaced_reactions_ids.txt';
+const compartmentFiles = fs.readdirSync('compartment/svg/').map((file)=> `compartment/svg/${file}`);
+const subsystemFiles = fs.readdirSync('subsystem/svg/').map((file)=> `subsystem/svg/${file}`);
+const filesToSearch = [...compartmentFiles, ...subsystemFiles];
+
+async function processLineByLine(fileToSearch) {
+    const notUpdatedIds = [];
+    const rl = readline.createInterface({
+    input: fs.createReadStream(fileToSearch),
+    output: process.stdout,
+    terminal: false
+    });
+  
+    for await (const line of rl) {
+        const match = line.match(/<g id="([^"]+)" class="rea">/);
+        if (match && match[1].indexOf('MA') == -1){
+            notUpdatedIds.push(match[1]);
+        };
+    };
+    return notUpdatedIds;
+};
+
+const getIds = async () => {
+    return await Promise.all(filesToSearch.map( async (fileToSearch) => {
+        const id = path.basename(fileToSearch);
+        const notUpdatedIds = await processLineByLine(fileToSearch);
+        const res = { [`${id}`]: notUpdatedIds };
+        return res;
+    }))
+};
+
+(async () => {
+    const notUpdatedIdsMapping = await getIds();
+    const merged = notUpdatedIdsMapping.reduce((all, current) => ({...all, ...current}), {});
+    fs.writeFile(fileResult, JSON.stringify(merged, null, 4), function (err) {
+        if (err) return console.log(err);
+        console.log('Writing result');
+    });
+})()
